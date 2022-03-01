0fd866ef863b67ae02061bc0676eca0712f9b486
BlueOompaLoompa <blueoompaloompa72@gmail.com>
2022-03-01 01:06:26 -0600

    Markdown article

--- src/lib/Article.svelte
+++ src/lib/Article.svelte
@@ -1,1 +1,27 @@
-<p>Test</p>
+<script>
+    import { onMount } from "svelte";
+    import {marked} from 'marked';
+
+    let content = 'Loading...'
+
+    onMount(async () => {
+        const res = await fetch('/article.md');
+        const text = await res.text();
+
+        content = marked.parse(text);
+    })
+</script>
+
+<article>
+{@html content}
+</article>
+
+<style>
+    article {
+        width: 100%;
+
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+</style>
